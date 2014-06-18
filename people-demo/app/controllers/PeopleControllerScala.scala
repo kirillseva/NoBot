bd0eec765e1591a6446d772d985c00fd27477021
Genia <trofimova.e@gmail.com>
2014-06-18 14:46:12 -0400

    adding Scala people_demo version

---
+++ people-demo/app/controllers/PeopleControllerScala.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+import play.api.libs.json._
+import play.api.libs.json.JsResult
+import models.People
+import scala.collection.JavaConverters._
+
+object PeopleControllerScala extends Controller {
+	def showAllPeople = Action { implicit request =>
+		val allPeople = People.getAll
+		//val matt = allPeople.length
+		val res = allPeople.asJava
+		//Ok(Json.obj("length" -> matt))
+		Ok(views.html.list(res))
+	}
+	
+//	def getOfficeByName = Action { implicit request =>
+		
+	
+//	}
+}
---
+++ people-demo/build.sbt
@@ -0,0 +1,14 @@
+name := "People_2"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  javaEbean,
+  cache,
+  jdbc,
+  anorm,
+  "mysql" % "mysql-connector-java" % "5.1.31"
+)     
+
+play.Project.playJavaSettings
