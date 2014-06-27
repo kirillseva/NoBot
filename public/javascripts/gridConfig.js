efbbd49f09944e4090ce54651701f23986276e23
Kirill Sevastyanenko <kirillseva@gmail.com>
2014-06-27 16:23:43 -0400

    added serializing gridster. now we need to figure out where to store it

---
+++ public/javascripts/gridConfig.js
@@ -0,0 +1,18 @@
+var gridster;
+
+$(function() {
+    gridster = $(".gridster > ul").gridster({
+        widget_margins: [5, 5],
+        widget_base_dimensions: [140, 140],
+        min_cols: 6
+    }).data('gridster');
+
+
+  $('.js-serialize').on('click', function() {
+      var s = gridster.serialize();
+
+      console.log(JSON.stringify(s));
+      $('#log').val(JSON.stringify(s));
+  })
+
+});
