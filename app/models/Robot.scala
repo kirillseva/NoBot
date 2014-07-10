0b2c038d86ceded2ea2182a99aea6c1b1001afe5
Kirill Sevastyanenko <kirillseva@gmail.com>
2014-07-10 19:46:30 -0400

    added robot model

---
+++ app/models/Robot.scala
@@ -0,0 +1,22 @@
+package models
+
+case class Location(x: Int, y: Int)
+
+case class Robot(location: Location)
+
+object Robot{
+
+  // just a stub for now. Ideally update all of the robot's aspects here
+  def updateInfo(): Robot {
+    val loc = Location.getLocation()
+    Robot(loc)
+  }
+}
+
+object Location{
+
+  //just a stub for now. Ideally will get the latest robot's location
+  def getLocation(): Location {
+    Location(14, 88)
+  }
+}
