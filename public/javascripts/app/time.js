7489a402a8b679bb38ef88d449875bc0d7074899
Kirill Sevastyanenko <kirillseva@gmail.com>
2014-08-04 00:59:02 -0400

    fixed clock widget for late night time

--- public/javascripts/app/time.js
+++ public/javascripts/app/time.js
@@ -11,6 +11,7 @@ function startTime() {
     var m = today.getMinutes();
     var s = today.getSeconds();
     // add a zero in front of numbers<10
+    h = checkTime(h);
     m = checkTime(m);
     s = checkTime(s);
     try {
